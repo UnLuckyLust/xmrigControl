4493263f4317e27bf5d21edc386b7ab50d71ba60
UnLuckyLust <unluckylust@gmail.com>
2023-12-12 15:56:41 +0200

    v1.4.0
    - Changed program name from UnLuckyMiner to xmrigControl

--- commands/install_xmrig_cuda.cmd
+++ commands/install_xmrig_cuda.cmd
@@ -1,7 +1,7 @@
-@REM Written by UnLuckyLust (https://DreamsWeaver.co) - https://github.com/UnLuckyLust/UnLuckyMiner
+@REM Written by UnLuckyLust (https://DreamsWeaver.co) - https://github.com/UnLuckyLust/xmrigControl
 @echo off
 cls
-echo [7;94m::: SETUP :::[0m[94m Installing XMrig Nvidia Cuda... [0m
+echo [7;94m::: SETUP :::[0m[94m Installing XMRig Nvidia Cuda v6.17.0... [0m
 curl --output xmrig-cuda.zip -LO https://github.com/xmrig/xmrig-cuda/releases/download/v6.17.0/xmrig-cuda-6.17.0-cuda11_4-win64.zip
 setlocal
 cd /d %~dp0
@@ -25,4 +25,4 @@ if exist xmrig-cuda.zip del xmrig-cuda.zip
 set xmrig_cuda_src=%cd%\xmrig-cuda-6.17.0
 for /f %%a IN ('dir "%xmrig_cuda_src%" /b') do move "%xmrig_cuda_src%\%%a" "%cd%\"
 rmdir %xmrig_cuda_src%
-call UnLuckyMiner.cmd
+call xmrigControl.cmd
